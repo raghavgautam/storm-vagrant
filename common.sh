d3ffcf4667a31312c3218008b33c85a19e60ce3c
Raghav Kumar Gautam <raghav@apache.org>
2016-04-29 13:08:13 -0700

    run user script if available

--- common.sh
+++ common.sh
@@ -1,3 +1,5 @@
+USER_SCRIPT="user-script.sh"
+{ [[ -f common.sh ]] && echo "Running ${USER_SCRIPT}" && bash ${USER_SCRIPT} } || echo "${USER_SCRIPT} not found, continuing."
 apt-get update
 apt-get --yes remove openjdk-6-jre-headless
 apt-get --yes install openjdk-7-jdk
