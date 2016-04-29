d1fb94c0400f808338ef317d771f8063344f0f9b
Raghav Kumar Gautam <raghav@apache.org>
2016-04-29 13:37:58 -0700

    fixing minor issue

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 USER_SCRIPT="user-script.sh"
-{ [[ -f common.sh ]] && echo "Running ${USER_SCRIPT}" && bash ${USER_SCRIPT} } || echo "${USER_SCRIPT} not found, continuing."
+[[ -f $USER_SCRIPT ]] && echo "Running ${USER_SCRIPT}" && bash ${USER_SCRIPT} || echo "${USER_SCRIPT} not found/executed, continuing."
 apt-get update
 apt-get --yes remove openjdk-6-jre-headless
 apt-get --yes install openjdk-7-jdk
